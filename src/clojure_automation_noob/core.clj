3231e1a4cbe740843649190e458135d7a1155bc7
Rahul Sharma <rsharma4944@altimetrik.com>
2019-03-15 18:44:23 +0530

    Chapter 5th exercise

--- src/clojure_automation_noob/core.clj
+++ src/clojure_automation_noob/core.clj
@@ -99,3 +99,52 @@
 
 ;exercise - 4 Write a function that will take your list of maps and convert it back to a
 (clojure.string/join "," (vals glitter-filter))
+
+; Chapter -5 Assignment
+
+; Exercise -1 
+(def character
+	{:name "Smooches McCutes"
+	:attributes {:intelligence 10
+	:strength 4
+	:dexterity 5}})
+
+(defn attr
+    "give nested attribute value"
+	[att-value]
+	(comp ((get-in character [:attributes])att-value)))
+	
+	;Example
+	;  (attr :intelligence)
+ 
+ 
+ ; Exercise 3 - Implement the assoc-in function. Hint: use the assoc function and
+ 
+ (defn my-assoc-in
+  [m [k & kws] v]
+  (if kws
+    (assoc m k (my-assoc-in (get m k) kws v))
+    (assoc m k v)))
+	
+	;Example
+    ;(my-assoc-in {} [:Profile :Automation :Skill] "Clojure")
+	;(my-assoc-in {} [:Profile] "Automation")
+
+; Exercise 2- comp implementation
+(defn my-comp
+	"my comp function"
+	[fn1 fn2]
+	(fn [& args]
+	(fn1 (reduce fn2 args))))
+
+ ;((my-comp + *) 8 7)
+ 
+ ; Exercise 4- use the update-in function.
+;update-in function.
+(def car {:name "Tata-Nexon" :avg 15 :Model 2018})
+(update-in car[:avg] + 10)
+
+; Results
+;#'user/car
+;{:name "Tata-Nexon", :avg 25, :Model 2018}
+
